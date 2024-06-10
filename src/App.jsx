79a9c9c43fd6d46a616dffcd5f484075305156a1
hdjoumdjeu <hermannpougoue1@gmail.com>
2024-06-10 13:16:53 -0500

    I cleaned up and added some code inside the App function on the App.jsx file
    I created a todoList array and initialized with three objects, each containing a id and a title

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,29 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+// 1- Above this App() function, I created an empty Array and store it in a variable named todoList.
+const todoList =[
+  {id: 1,title:"Complete assignment"},
+  {id: 2,title:"Attend at least one mentor session weekly"},
+  {id: 3,title:"Do more Project"}
+];
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
+   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+          <h1>Todo List</h1>
+            <ul>
+                   {/* Inside the JavaScript expression, map over your todoList array */ }
+                      {
+                        todoList.map((list_item)=>(
+                          // here i am getting the list item for each object in my todoList array
+                          <li key ={list_item.id}>{list_item.title}.</li>
+                        ))                  
+                      }
+            </ul>
+      </div>     
     </>
-  )
+  );
 }
 
 export default App
