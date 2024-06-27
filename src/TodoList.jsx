804d3d071d02735c8b273dbb0fa15bf8a04204a5
hdjoumdjeu <hermannpougoue1@gmail.com>
2024-06-27 12:07:31 -0500

    Implementation of the lesson 1-3

--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -1,4 +1,5 @@
 
+import TodoListItem from './TodoListItem'
 
 // 1- Above this App() function, I created an array with some initial data and store it in a variable named todoList.
 const todoList =[
@@ -9,14 +10,12 @@ const todoList =[
 function TodoList(){
 
     return (
-        <ul>
-                   {/* Inside the JavaScript expression, map over your todoList array */ }
-                      {
-                        todoList.map((list_item)=>(
-                          // here i am getting the list item for each object in my todoList array
-                          <li key ={list_item.id}>{list_item.title}.</li>
-                        ))                  
-                      }
+            <ul>  
+              {
+                  todoList.map((item)=>{
+                    <TodoListItem key={item.id} todo={item.title}/>
+                })
+              }                     
             </ul>
 
     );
