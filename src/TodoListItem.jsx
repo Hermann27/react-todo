0b0001d8dd53b4e33bf658cb2355e54cbf010ef4
hdjoumdjeu <hermannpougoue1@gmail.com>
2024-07-31 09:55:33 -0500

    I have used prettier to well format the code.

--- src/TodoListItem.jsx
+++ src/TodoListItem.jsx
@@ -1,9 +1,11 @@
-function TodoListItem({id, title,onRemoveTodo}){
-        return(
-                <li>               
-                        {title}
-                        <button type="button" onClick={() => onRemoveTodo(id)}>Remove</button>
-                </li>              
-        ); 
+function TodoListItem({ id, title, onRemoveTodo }) {
+  return (
+    <li>
+      {title}
+      <button type="button" onClick={() => onRemoveTodo(id)}>
+        Remove
+      </button>
+    </li>
+  );
 }
 export default TodoListItem;
